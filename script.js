da6a0dbc91b157ba3b29abea1802ecfab0a04dd3
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-05-11 15:46:06 -0400

    feat: create library array and Book constructor
    - Added a myLibrary array to store book objects
    - Defined a Book constructor function
    - Constructor accepts id, title, author, number of pages, and read status (boolean)

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book(id, title, author, pages, hasRead) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
+}
+
