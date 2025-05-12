a2134dbc3eaaf0027f67e15634260f2a29588e90
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-05-12 16:03:08 -0400

    feat: add formHandler and addBookToLibrary functions
    - Added formHandler function to retrieve form input values and call addBookToLibrary
    - Implemented addBookToLibrary to create a new Book object and store it in myLibrary
    - Removed id parameter from Book constructor
    - Added event listener to button to trigger formHandler on click

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const myLibrary = [];
 
-function Book(id, title, author, pages, hasRead) {
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => formHandler());
+
+function Book (title, author, pages, hasRead) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.hasRead = hasRead;
+}
+
+function formHandler () {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector('#pages').value;
+  const hasRead = document.querySelector('#status').checked; 
+  addBookToLibrary(title, author, pages, hasRead)
+}
+
+function addBookToLibrary (title, author, pages, hasRead) {
+  myLibrary.push(new Book(title, author, pages, hasRead));
+  console.log(myLibrary)
 }
