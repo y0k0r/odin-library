53a3dc00ce6867f70037c7c9047adb0f15bded3f
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-05-16 22:17:31 -0400

    feat: add functionality to remove books from library
    - Add "Remove Book" button to each book card
    - Set data-id attribute on cards and remove buttons using book ID
    - Implment logic to identify and remove the correct book from the myLibary array and update the UI

--- script.js
+++ script.js
@@ -38,27 +38,49 @@ function displayBooks () {
   cardsDiv.innerHTML='';
 
   for (book of myLibrary) {
-    const div = document.createElement("div");
-    div.classList.add("card");
+    const card = document.createElement("div");
+    card.classList.add("card");
 
     const bookTitle = document.createElement("p");
     const bookAuthor = document.createElement("p");
     const bookPages = document.createElement("p");
     const bookStatus = document.createElement("p");
-    bookTitle.innerText = `Title: ${book.title}`
-    bookAuthor.innerText = `Author: ${book.author}`
-    bookPages.innerText = `Pages: ${book.pages}`
+    const removeBtn = document.createElement("button");
+    bookTitle.innerText = `Title: ${book.title}`;
+    bookAuthor.innerText = `Author: ${book.author}`;
+    bookPages.innerText = `Pages: ${book.pages}`;
     if (book.hasRead) {
-      bookStatus.innerText = `Status: Read`
+      bookStatus.innerText = `Status: Read`;
     } else {
-      bookStatus.innerText = `Status: Not Read`
+      bookStatus.innerText = `Status: Not Read`;
     }
 
-    div.appendChild(bookTitle);
-    div.appendChild(bookAuthor);
-    div.appendChild(bookPages);
-    div.appendChild(bookStatus);
-    cardsDiv.appendChild(div);
+    removeBtn.innerText = "Remove Book";
+    removeBtn.dataset.id = `${book.id}`;
+
+    removeBtn.addEventListener("click", () => {
+      let index = 0;
+      for (let book of myLibrary) {
+        if (book.id === removeBtn.dataset.id) {
+          console.log(`${index}: ${book.id}`)
+          myLibrary.splice(index, 1);
+          displayBooks();
+          break;
+        }
+        index++;
+          }
+    })  
+
+    card.dataset.id = `${book.id}`;
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookStatus);
+    card.appendChild(removeBtn);
+    cardsDiv.appendChild(card);
   }
 }
+
+ 
+
 
