6dc5ebdc6f4e75674a101096eaecf6e3b6249ba8
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-05-18 15:32:37 -0400

    chore: remove old submit button event listener
    - Cleanup leftover submitBtn code now handled by form submit event

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const myLibrary = [];
 
 const dialog = document.querySelector("dialog");
 const cardsDiv = document.querySelector(".cards");
-const submitBtn = document.querySelector(".submitButton");
 const addBookBtn = document.querySelector(".addBook");
 const closeBtn = document.querySelector(".closeButton");
-/*submitBtn.addEventListener("click", () => formHandler());*/
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
